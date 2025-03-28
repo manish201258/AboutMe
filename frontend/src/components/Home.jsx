e489c89b74ce740d4d8b8c089133489c1c2228c2
manish201258 <manish201258@gmail.com>
2025-03-28 16:19:42 +0530

    code update

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,45 +1,34 @@
-import React, { useEffect, useState } from 'react'
-import Profile from './Profile'
-import AboutMe from './AboutMe'
-import Skills from './Skills'
-import Education from './Education'
-import Experience from './Experience'
-import Contact from './Contact'
-import Projects from './Projects'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import MobileMenu from './MobileMenu'
- 
-const Home = () => {
+import React from 'react';
+import Profile from './Profile';
+import AboutMe from './AboutMe';
+import Skills from './Skills';
+import Education from './Education';
+import Experience from './Experience';
+import Contact from './Contact';
+import Projects from './Projects';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import MobileMenu from './MobileMenu';
+import { Analytics } from "@vercel/analytics/react";
 
+const Home = () => {
   return (
     <>
-    <div className='home'>
-      <div>
-        Visitors
-      </div>
-    <Navbar />
-          <Profile />
-        
-          <AboutMe />
-        
-          <Skills />
-          
-          <Education />
-        
-          <Experience />
-        
-          <Projects />
-        
-          <Contact />
+      <div className='home'>
+        <Navbar />
+        <Profile />
+        <AboutMe />
+        <Skills />
+        <Education />
+        <Experience />
+        <Projects />
+        <Contact />
         <Footer />
-    </div>
-    <div>
-
-      <MobileMenu/>
-    </div>
+      </div>
+      <MobileMenu />
+      <Analytics />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
